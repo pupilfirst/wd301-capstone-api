a9defe0fcf942e0190f03f4caeccb5d0ff7c2a36
Kaiser Sakhi <kaisersakhi@gmail.com>
2023-05-30 17:32:32 +0530

    Refactor: max scoring for different sports

--- app/helpers/live_score_helper.rb
+++ app/helpers/live_score_helper.rb
@@ -47,8 +47,21 @@ class LiveScoreHelper
     check_timing(match)
     time_elapsed = (Time.now - match.start_at) / 60
     seeds = saved_seed(match)
-    s1 = map_range(time_elapsed + seeds.first, 0, 120, 0, 15).to_i.to_s
-    s2 = map_range(time_elapsed + seeds.last, 0, 120, 0, 15).to_i.to_s
+    sport_type = match.teams.first.sport.sport_type
+    new_max = case sport_type
+                     when Sport.sport_types[:cricket]
+                       300
+                     when Sport.sport_types[:basketball]
+                       120
+                     when Sport.sport_types[:table_tennis]
+                       11
+                     when Sport.sport_types[:rugby]
+                       25
+                     else
+                       15
+                     end
+    s1 = map_range(time_elapsed + seeds.first, 0, 120, 0, new_max).to_i.to_s
+    s2 = map_range(time_elapsed + seeds.last, 0, 120, 0, new_max).to_i.to_s
     "#{s1},#{s2}"
   end
 end
